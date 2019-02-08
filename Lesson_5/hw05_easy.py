f8765273cf0c31c3391ae44828201590be32887e
Varvara <poplavskayavarvara@gmail.com>
2019-02-08 15:55:00 +0400

    5 урок задания изи

--- Lesson_5/hw05_easy.py
+++ Lesson_5/hw05_easy.py
@@ -3,12 +3,40 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+import os
+
+def create_dirs():
+    '''
+    Создает директории в текущей папке
+    '''
+    for i in range(1,10):
+        try:
+            os.mkdir(os.path.join(os.getcwd(), 'dir_' + str(i)))
+        except FileExistsError:
+            print('Already exists!')
+
+def delete_dirs():
+    '''
+    Удаляет директории в текущей папке
+    '''
+    for i in range(1,10):
+        try:
+            os.rmdir(os.path.join(os.getcwd(), 'dir_' + str(i)))
+        except FileNotFoundError:
+            print('Already doesn\'t exist!')
+
+create_dirs()
+delete_dirs()
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 #ТОЛЬКО ПАПКИ, А НЕ ФАЙЛЫ!
 
+list = [print(i) for i in os.listdir(os.getcwd()) if os.path.isdir(os.path.join('.', i))]
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
 # ИСПОЛЬЗОВАТЬ ТОЛЬКО МОДУЛЬ OS
+'''
+os.link(тут путь до текущего файла, os.getcwd()))
+'''
